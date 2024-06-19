7a54b42ffabfba2f970ca3d39ef742fb45513e50
Vikash Prem Sharma <vikashkumarukg977@gmail.com>
2024-06-20 03:54:19 +0530

    real time update functionality implemented

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -13,6 +13,10 @@ const CourseList = () => {
 
   useEffect(() => {
     dispatch(fetchCourses());
+    const intervalId = setInterval(() => {
+      dispatch(fetchCourses());
+    }, 100);
+    return () => clearInterval(intervalId);
   }, [dispatch]);
 
   // Update filteredCourses whenever courseList changes
@@ -43,7 +47,7 @@ const CourseList = () => {
   return (
 
     <>
-    <div className="text-center mt-7">
+      <div className="text-center mt-7">
         <input
           type="text"
           placeholder="Search courses..."
@@ -52,33 +56,34 @@ const CourseList = () => {
           className="shadow appearance-none border border-black rounded w-1/2 py-2 px-3 text-gray-700"
         />
       </div>
-    <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4">Course List</h2>
-      <div className='overflow-y-auto max-h-screen'>
-        {filteredCourses && filteredCourses.length > 0 ? (
-          <ul className="space-y-4">
-            {filteredCourses.map(course => (
-              <li key={course.id} className=" shadow-md rounded-md p-4 md:flex bg-gray-200 block">
-                <img src={course.thumbnail} alt={course.name} className="w-60 h-60 rounded-md mr-4 md:text-center p-5" />
-                <div className="flex-1 w-fit md:w-1/2 p-5">
-                  <h3 className="text-xl font-semibold">{course.name}</h3>
-                  <p><strong>Instructor:</strong> {course.instructor}</p>
-                  <p><strong>Description:</strong> {course.description}</p>
-                  <p><strong>Enrollment Status:</strong> {course.enrollmentStatus}</p>
-                  <p><strong>Duration:</strong> {course.duration}</p>
-                  <p><strong>Schedule:</strong> {course.schedule}</p>
-                  <p><strong>Location:</strong> {course.location}</p>
-                  <p><strong>Prerequisites:</strong> {course.prerequisites.join(', ')}</p>
-                  <Link to={`/courses/${course.id}`} className="text-blue-500 hover:underline">View Details</Link>
-                </div>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p className="text-gray-500">No courses available.</p>
-        )}
+      <div className="container mx-auto p-4">
+        <h2 className="text-2xl font-bold mb-4">Course List</h2>
+        <div className='overflow-y-auto max-h-screen'>
+          {filteredCourses && filteredCourses.length > 0 ? (
+            <ul className="space-y-4">
+              {filteredCourses.map(course => (
+                <li key={course.id} className=" shadow-md rounded-md p-4 md:flex bg-gray-200 block">
+                  <img src={course.thumbnail} alt={course.name} className="w-60 h-60 rounded-md mr-4 md:text-center p-5" />
+                  <div className="flex-1 w-fit md:w-1/2 p-5">
+                    <h3 className="text-xl font-semibold">{course.name}</h3>
+                    <p><strong>Instructor:</strong> {course.instructor}</p>
+                    <p><strong>Description:</strong> {course.description}</p>
+                    <p><strong>Enrollment Status:</strong> {course.enrollmentStatus}</p>
+                    <p><strong>Duration:</strong> {course.duration}</p>
+                    <p><strong>Schedule:</strong> {course.schedule}</p>
+                    <p><strong>Location:</strong> {course.location}</p>
+                    <p><strong>Prerequisites:</strong> {course.prerequisites.join(', ')}</p>
+                    <p>Number of likes: {course.like}</p>
+                    <Link to={`/courses/${course.id}`} className="text-blue-500 hover:underline">View Details</Link>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-gray-500">No courses available.</p>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 };
